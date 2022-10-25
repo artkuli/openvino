bf6212175642e845d88bd05dc2797b3c38898bda
Artur Kulikowski <artur.kulikowski@intel.com>
2022-10-25 12:40:31 +0200

    Nlohmann_json and json-schema-validator as a libraries from Conan

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,9 @@
 from conans import ConanFile, CMake
-# from conan.tools.cmake import CMakeToolchain
 
 
 class OpenvinoConan(ConanFile):
     name = "openvino"
-    version = "0.1"
+    version = "2022.2"
 
     # Optional metadata
     license = "<Put the package license here>"
@@ -17,7 +16,7 @@ class OpenvinoConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
-    generators = ["CMakeToolchain", "CMakeDeps", "cmake_paths"]
+    generators = ["cmake", "cmake_find_package", "CMakeDeps", "cmake_paths", "CMakeToolchain"]
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "cmake/*",  "ngraph/*", "scripts/*", "src/*", "thirdparty/*", "tools/*", "samples/*", "docs/*", "licensing/*", "tests/*", "CMakeLists.txt"
@@ -26,9 +25,9 @@ class OpenvinoConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    # def requirements(self):
-    #     self.requires("protobuf/3.21.4")
-    #     self.requires("onnx/1.11.0")
+    def requirements(self):
+        # Don't need nlohmann_json because json-schema-validator requires it
+        self.requires("json-schema-validator/2.1.0")
 
     # def layout(self):
     #     cmake_layout(self)
@@ -41,7 +40,6 @@ class OpenvinoConan(ConanFile):
         cmake = CMake(self)
         cmake.verbose = True
         cmake.definitions["ENABLE_INTEL_CPU"] = "ON"
-        cmake.definitions["ENABLE_INTEL_CPU"]="ON" 
         cmake.definitions["ENABLE_INTEL_GPU"]="OFF" 
         cmake.definitions["ENABLE_INTEL_GNA"]="OFF" 
         cmake.definitions["ENABLE_INTEL_MYRIAD"]="OFF" 
@@ -52,12 +50,11 @@ class OpenvinoConan(ConanFile):
         cmake.definitions["ENABLE_PROFILING_ITT"]="OFF" 
         cmake.definitions["ENABLE_SAMPLES"]="OFF" 
         cmake.definitions["ENABLE_PYTHON"]="OFF" 
-        # cmake.definitions["PYTHON_EXECUTABLE"] ="/usr/bin/python3" 
         cmake.definitions["ENABLE_CPPLINT"]="OFF" 
         cmake.definitions["ENABLE_NCC_STYLE"]="OFF" 
         cmake.definitions["ENABLE_OV_PADDLE_FRONTEND"]="OFF" 
         cmake.definitions["ENABLE_OV_TF_FRONTEND"]="OFF" 
-        cmake.definitions["ENABLE_OV_ONNX_FRONTEND"]="ON" 
+        cmake.definitions["ENABLE_OV_ONNX_FRONTEND"]="OFF" 
         cmake.definitions["CMAKE_EXPORT_NO_PACKAGE_REGISTRY"] = "OFF"
         cmake.definitions["ENABLE_TEMPLATE"] = "OFF"
         cmake.definitions["ENABLE_INTEL_MYRIAD_COMMON"] = "OFF"
